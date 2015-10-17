afc7cb704834ea3f4cd509099728ee40c56b6ead
khank551 <khank551@students.bju.edu>
2015-10-16 23:54:04 -0400

    Starting Code

---
+++ SpaceAlien.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-10-16T23:29:16
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = SpaceAlien
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ mainwindow.cpp
@@ -0,0 +1,49 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QKeyEvent>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *event)
+{
+    switch (event->key())
+    {
+    case Qt::Key_Up:
+        if (ui->lblMove->y() >= 5)
+        {
+            ui->lblMove->move((ui->lblMove->x()), (ui->lblMove->y() - 5));
+        }
+        break;
+    case Qt::Key_Down:
+        if (ui->lblMove->y() <= (ui->centralWidget->height() - 20))
+        {
+            ui->lblMove->move((ui->lblMove->x()), (ui->lblMove->y() + 5));
+        }
+        break;
+    case Qt::Key_Left:
+        if (ui->lblMove->x() >= 5)
+        {
+            ui->lblMove->move((ui->lblMove->x() - 5), (ui->lblMove->y()));
+        }
+        break;
+    case Qt::Key_Right:
+        if (ui->lblMove->x() <= (ui->centralWidget->width() - 70))
+        {
+            ui->lblMove->move((ui->lblMove->x() + 5), (ui->lblMove->y()));
+        }
+        break;
+    default:
+        break;
+    }
+}
