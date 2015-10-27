d21a6420fd385a00f4da5fb08ccac19699152bd5
acuen753 <acuen753@students.bju.edu>
2015-10-27 15:16:35 -0400

    Enemy stuff

--- enemyspawn.cpp
+++ enemyspawn.cpp
@@ -1,1 +1,29 @@
+#include <QString>
+#include <QLabel>
+#include <QDebug>
+#include <string>
+#include <random>
+
+using namespace std;
+
+int random_int(int min, int max) {
+    static std::default_random_engine engine { std::random_device{}() };
+    std::uniform_int_distribution<int> distro{min, max};
+    return distro(engine);
+}
+
+void enemyGen()
+{
+    /*auto label_left = random_int(0, this->geometry().width() - 32);
+    auto label_top = random_int(ui->btnStart->geometry().bottom(),
+                   this->geometry().height() - 32);
+
+    Enemy *alien = new Enemy(ui->widgetView);
+    QPixmap evil(":/images/asteroid.png");
+    alien->setPixmap(evil);
+    alien->setGeometry(QRect(label_left, label_top, 32, 32));
+    alien->setScaledContents(true);
+    alien->show(); */
+}
+
 
--- enemyspawn.h
+++ enemyspawn.h
@@ -1,4 +1,30 @@
 #ifndef ENEMYSPAWN_H
 #define ENEMYSPAWN_H
 
-#endif // ENEMYSPAWN_H
+#include <QString>
+#include <QLabel>
+#include <QDebug>
+#include <string>
+#include <random>
+
+using namespace std;
+
+class Enemy : public QLabel
+{
+    Q_OBJECT
+
+public:
+
+    explicit Enemy(QWidget *parent): QLabel(parent) {
+
+    }
+
+
+//add some fun stuff here!
+
+
+};
+//Enemy Class
+
+
+#endif
