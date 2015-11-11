4373c48ee3c752e8642e91241e303568e6f34b1e
khank551 <khank551@students.bju.edu>
2015-11-10 19:25:06 -0500

    Fixed Enemy class

--- enemyspawn.h
+++ enemyspawn.h
@@ -7,30 +7,47 @@
 #include <string>
 #include <random>
 
-using namespace std;
+#include <string>
 
-class Enemy : public QLabel
+class Enemy
+{
+private:
+    int id; // The unique id of the enemy
+    int x, y; // x, y = current location
+    int dX, dY; // deltaX, deltaY = the increase/decrease every tick
+public:
+    Enemy(int startX, int startY, int deltaX, int deltaY, int newId): x(startX), y(startY), dX(deltaX), dY(deltaY), id(newId) { }
+
+    // Accessor methods
+    int getX() { return x; }
+    int getY() { return y; }
+    int getDeltaX() { return dX; }
+    int getDeltaY() { return dY; }
+    int getID() { return id; }
+
+    std::string getSave();
+
+    // Updates the enemy's position
+    void move();
+};
+
+class EnemyLabel : public QLabel
 {
     Q_OBJECT
 
+    Enemy *myEnemy;
+
 public:
 
-    int dX, dY;
+    explicit EnemyLabel(QWidget *parent): QLabel(parent) { }
+    Enemy *getEnemy() { return myEnemy; }
+    void setEnemy(Enemy *newEnemy) { myEnemy = newEnemy; }
 
-    explicit Enemy(QWidget *parent, int initX, int initY): QLabel(parent) {
+    void enemyGen();
 
-       dX = initX;
-       dY = initY;
-    }
-    void enemyGen(QPixmap evil, Enemy *alien, int label_left, int label_top);
-    void updateEnemy(Enemy *lblEnemy);
-
-
- int getDeltaX() { return dX; }
- int getDeltaY() { return dY; }
-
+    ~EnemyLabel() { }
 };
-//Enemy Class
+//EnemyLabel Class
 
 
 #endif
